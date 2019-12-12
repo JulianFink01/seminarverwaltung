ef3e575145df44341cb4442d93fd1a42ee96a144
MatRob0300 <matharobert1@gmail.com>
2019-12-12 14:13:32 +0100

    -new ordner images + two images; -new ordner model + funktion.inc.php; -new ordner views + login_verwaltungsperson.tpl.php; -in index.php new switch-case

--- backend/index.php
+++ backend/index.php
@@ -1,4 +1,18 @@
 
 <?php
 echo "test1234";
+require_once '../db.php';
+    if (isset ($_REQUEST['aktion']))
+        $aktion = $_REQUEST['aktion'];
+    else
+        $aktion = 'verwaltungsperson_login';
+    switch($aktion) {
+    	case "verwaltungsperson_login":
+          $aktion = 'login_verwaltungsperson';
+    		break;
+
+
+    }
+
+    require_once 'views/' . $aktion . '.tpl.php';
 ?>
---
+++ backend/views/login_verwaltungsperson.tpl.php
@@ -0,0 +1,24 @@
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="styles/main-style.css">
+  <link rel="stylesheet" type="text/css" href="styles/login-style.css">
+</head>
+<body>
+  <div id="login">
+
+      <div class="login-item">
+          <img src="Images/login-icon.png" alt="login-image">
+      </div>
+
+      <div class="login-item">
+
+        <form id="login-form">
+          <legend>Verwaltungsperson - Login</legend>
+          <input type="text" placeholder="Personal Key" name="key" required>
+          <input type="password" placeholder="Passwort" name="passwd" required>
+          <input type="submit" value="Login">
+        </form>
+
+      </div>
+  </div>
+</body>
