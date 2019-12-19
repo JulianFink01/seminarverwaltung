c27644b98ab0ba1bea7e4beede8e970cd9574798
MatRob0300 <matharobert1@gmail.com>
2019-12-19 15:47:36 +0100

    teilnehmer

--- entities/teilnehmer.php
+++ entities/teilnehmer.php
@@ -3,6 +3,48 @@
 
 class Teilnehmer{
 
+$id = 0;
+$vorname = "";
+$nachname = "";
+$email = "";
+$token = "";
+
+private __construcor(){
+
+}
+private void function setID($id){
+   $this->id = $id;
+}
+private function getID(){
+  return $this->id;
+}
+private void function setVorname($vorname){
+   $this->vorname = $vorname;
+}
+private function getVorname(){
+  return $this->vorname;
+}
+private void function setNachname($nachname){
+   $this->nachname = $nachname;
+}
+private function getNachname(){
+  return $this->nachname;
+}
+private void function setID($email){
+   $this->email = $email;
+}
+private function getEmail(){
+  return $this->email ;
+}
+private void function setToken($token){
+   $this->token = $token;
+}
+private function getToken(){
+  return $this->token;
+}
+
+
+
 
 }
 
