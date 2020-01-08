ddf7f5f513199b2d6ce84facb59d463f86e24b84
Julian Fink <julianfink01@gmail.com>
2020-01-08 11:51:24 +0100

    Working on db classes

--- entities/kurs.php
+++ entities/kurs.php
@@ -2,7 +2,33 @@
 
 class Kurs{
 
+$id = 0;
+$datum = "";
+$titel = "";
+$maxTeilnehmer = 0;
+$referent = "";
+$beschreibung = "";
+$ort_raum = "";
+$kontakt = "";
+$von = "";
+$bis = "";
+$unterschriftsliste_zweispaltig = false;
+$koordination = "";
+$anmeldeschluss = "";
+$fortbildung_id = 0;
+$dauer = 0;
 
+
+  public function setId($id){
+    $this->id = $id;
+  }
+  public function getId(){
+    return $this->id;
+  }
+  public function setDatum($datum){
+    $this->datum = $datum;
+  }
+  public function getDatum($)
 }
 
 
