b44bd2341bc0f3338261ff562c09ba265f7c1d5e
hoferjulian <julianhofer2712@gmail.com>
2020-01-09 14:29:33 +0100

    Fortibildung Hofer | 09/01/2020

--- entities/fortbildung.php
+++ entities/fortbildung.php
@@ -2,7 +2,29 @@
 
 class Fortbildung{
 
+$id = 0;
+$name = "";
+$status = false;
 
+
+public function setId($id){
+  $this->id = $id;
+}
+public function getId(){
+  return $this->id;
+}
+public function setName($name){
+  $this->name = $name;
+}
+public function getName(){
+  return $this->name;
+}
+public function setStatus($status){
+  $this->status = $status;
+}
+public function getStatus(){
+  return $this->status;
+}
 }
 
 
