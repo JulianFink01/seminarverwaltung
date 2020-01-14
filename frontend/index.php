d0e1b721f14a47f9b703d3cf91cd3d64a6eca4ce
Julian Fink <julianfink01@gmail.com>
2020-01-14 13:26:35 +0100

    Login with Token now available

--- frontend/index.php
+++ frontend/index.php
@@ -1,11 +1,22 @@
 <?php
     require_once("../entities/db.php");
+    require_once("../entities/fortbildung.php");
+    require_once("../entities/kurs.php");
+    require_once("../entities/nimmtteil.php");
+    require_once("../entities/teilnehmer.php");
 
 
     if (isset ($_REQUEST['aktion']))
         $aktion = $_REQUEST['aktion'];
-    else
-        $aktion = 'show_login';
+        else
+            $aktion = 'show_login';
+
+    if (isset ($_REQUEST['token'])){
+            $token = $_REQUEST['token'];
+            $user = Teilnehmer::findeNachToken($token);
+            $aktion = 'show_Seminare';
+            echo $user->__toString();
+            }
 
     switch($aktion) {
         case "show_login":
