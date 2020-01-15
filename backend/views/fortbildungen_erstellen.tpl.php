1e55e275f6fce1abe17e3c8a960427eeb510c4cf
MatRob0300 <matharobert1@gmail.com>
2020-01-15 19:55:17 +0100

    erstellt: fortbildung_erstellen-style.css + fortbildung_erstellen.tpl.php änderung

---
+++ backend/views/fortbildungen_erstellen.tpl.php
@@ -0,0 +1,23 @@
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="styles/fortbildung_erstellen-style.css">
+</head>
+<body>
+  <div id="gesamt">
+  <header id="kopf">
+
+            <h1>Fortbildungen - Verwaltung</h1>
+
+  </header>
+<main>
+<div id="inhalt">
+
+</div>
+</main>
+<!--<footer id="fuss">
+            Footer
+        </footer>-->
+
+</div>
+
+</body>
