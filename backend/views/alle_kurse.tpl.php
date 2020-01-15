c651e5b42d25b83d59ade4f286163a21ac5a2114
MatRob0300 <matharobert1@gmail.com>
2020-01-15 21:24:21 +0100

    bearbeitung: alle_kurse.tpl.php + alle_kurse-style.css

--- backend/views/alle_kurse.tpl.php
+++ backend/views/alle_kurse.tpl.php
@@ -1,20 +1,45 @@
 <head>
+  <!-- https://t3n.de/news/css3-dynamische-tabs-ohne-365861/-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="styles/alle_kurse-style.css">
 </head>
 <body>
-  <header id="kopf"></header>
-  <div id="leiste"></div>
+  <header id="kopf">
+<h1>Fortbildungen - Verwaltung</h1>
+  </header>
 
-  <div id="suchleiste">
-<input type="search" placeholder="Suche nach Kurs" list="kurssuche"/>
-<datalist id="kurssuche">
-  <option>A</option>
-  <option>A</option>
-</datalist>
+  <div id="login_Feld"></div>
+
+  <div id="leiste">
+    <article class="infobox">
+        <section id="allgemeiner">
+            <h2><a href="#allgemeiner">Kurse</a></h2>
+
+            <main id="kurse_anzeigen">
+            </main>
+
+          <!--  <div id="suchleiste">
+          <input type="search" placeholder="Suche nach Kurs" list="kurssuche"/>
+          <datalist id="kurssuche">
+            <option>A</option>
+            <option>A</option>
+          </datalist>
+        </div>-->
+
+            <aside id="kreisbutton"></aside>
+
+        </section>
+        <section id="funktionen">
+            <h2><a href="#funktionen">Teilnehmer</a></h2>
+
+        </section>
+        <section id="preise">
+            <h2><a href="#preise">E-Mail senden</a></h2>
+
+        </section>
+    </article>
   </div>
 
-  <aside id="kreisbutton"></aside>
-  <main id="inhalt"></main>
+
 
 </body>
