86798e78f5f3517afd63956cd0d8e63f3b42201f
SimonMairhofer <simon321mairhofer@gmail.com>
2020-01-16 15:14:04 +0100

    teilnehmer-style.css erstellt und in kurse.tpl.php hinzugefügt

--- backend/views/kurse.tpl.php
+++ backend/views/kurse.tpl.php
@@ -2,6 +2,7 @@
   <!-- https://t3n.de/news/css3-dynamische-tabs-ohne-365861/-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="styles/kurse-style.css">
+  <link rel="stylesheet" type="text/css" href="styles/teilnehmer-style.css">
 </head>
 <body>
   <header id="kopf">
