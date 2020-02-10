f5084f7a4f590278d72146cbfc713e5bdfbf9986
MatRob0300 <matharobert1@gmail.com>
2020-02-10 13:42:49 +0100

    controller verschoben

--- backend/index.php
+++ backend/index.php
@@ -1,15 +1,24 @@
 
 <?php
 
-require_once 'models/entities/DB.php';
-require_once 'models/entities/fortbildung.php';
-require_once 'models/entities/kurs.php';
-require_once 'backend/models/PHPMail-master..?';
-require_once 'model/entities/seminartermin.php';
-require_once 'model/entities/benutzer.php';
-require_once 'model/entities/seminar.php';
 
-require_once 'controller/Controller.php';
+
+require_once 'entities/DB.php';
+require_once 'entities/fortbildung.php';
+require_once 'entities/kurs.php';
+require_once 'entities/nimmtteil.php';
+require_once 'entities/teilnehmer.php';
+require_once 'entities/test.php';
+
+require_once 'models/fortbildung.php';
+require_once 'models/funktionen.inc.php';
+require_once 'models/kurs.php';
+
+require_once 'Controller/controller.php';
+
+
+
+
 
 
 $aktion = isset($_GET['aktion'])?$_GET['aktion']:'alleST';
@@ -20,37 +29,5 @@ if (method_exists($controller, $aktion)){
         $controller->run($aktion);
 }
 
-/*
-    if (isset ($_REQUEST['aktion']))
-        $aktion = $_REQUEST['aktion'];
-    else
-        $aktion = 'hauptseite';
-    switch($aktion) {
-    	case "show_login":
-          $aktion = 'login_verwaltung';
-    		break;
-      case "fortbildung":
-          $aktion = 'fortbildungen';
-        break;
-      case "hauptseite":
-        $aktion = 'hauptseite';
-        break;
-      case "kurse":
-        $aktion = "kurse";
-        break;
-     case "k_erstellen":
-        $aktion = "kurse_erstellen";
-        break;
-     case "f_erstellen":
-      $aktion = "fortbildungen_erstellen";
-      break;
-    case "send_email":
-      $aktion = "send_email";
-      send_email();
 
-
-    }
-
-    require_once 'views/' . $aktion . '.tpl.php';
-    */
 ?>
