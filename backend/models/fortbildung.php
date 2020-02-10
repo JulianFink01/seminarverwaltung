6846cb7a5c2a80cbecdf3956af66449d44755f99
MatRob0300 <matharobert1@gmail.com>
2020-02-10 09:10:59 +0100

    fortbildung.php änderunungen

--- backend/models/fortbildung.php
+++ backend/models/fortbildung.php
@@ -2,7 +2,7 @@
 require_once('../entities/fortbildung.php');
 $fortbildungen = Fortbildung::findeAlle();
 
-var_dump("dsklfjsdölkfj");
+
   if(isset($_POST['submit'])){
     $fortbildungen->setName($_POST['titel']);
     $fortbildungen->setStatus('1');
