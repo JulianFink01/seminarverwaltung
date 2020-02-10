11bf65c201be51fd6335effec5696d56f9bcda22
MatRob0300 <matharobert1@gmail.com>
2020-02-10 09:46:05 +0100

    controller erstellt und bearbeitet

--- backend/index.php
+++ backend/index.php
@@ -1,8 +1,26 @@
 
 <?php
-require_once '../entities/db.php';
-require_once 'models/funktionen.inc.php';
 
+require_once 'models/entities/DB.php';
+require_once 'models/entities/fortbildung.php';
+require_once 'models/entities/kurs.php';
+require_once 'backend/models/PHPMail-master..?';
+require_once 'model/entities/seminartermin.php';
+require_once 'model/entities/benutzer.php';
+require_once 'model/entities/seminar.php';
+
+require_once 'controller/Controller.php';
+
+
+$aktion = isset($_GET['aktion'])?$_GET['aktion']:'alleST';
+
+$controller = new Controller();
+
+if (method_exists($controller, $aktion)){
+        $controller->run($aktion);
+}
+
+/*
     if (isset ($_REQUEST['aktion']))
         $aktion = $_REQUEST['aktion'];
     else
@@ -34,4 +52,5 @@ require_once 'models/funktionen.inc.php';
     }
 
     require_once 'views/' . $aktion . '.tpl.php';
+    */
 ?>
