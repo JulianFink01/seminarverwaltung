5b8a72b47006ac8b4c13b7593e39d5571e54e37d
Lukas-Roccasalvo <roccasalvo.lukas@gmail.com>
2020-02-11 15:49:30 +0100

    send email

--- backend/models/funktionen.inc.php
+++ backend/models/funktionen.inc.php
@@ -6,30 +6,32 @@ function send_email() {
 
     require_once 'PHPMailer-master/src/PHPMailer.php';
     $mailer = new \PHPMailer\PHPMailer\PHPMailer();
-    echo $_REQUEST['fortbildung_id'];
-    $teilnehmer = NimmtTeil::findeAlleFortbildungTeilnehmer(Fortbildung::finde($_REQUEST['fortbildung_id']));
+
+    $fortbildung = Fortbildung::finde($_REQUEST['fortbildung_id']);
+    $teilnehmer = NimmtTeil::findeAlleFortbildungTeilnehmer($fortbildung);
+    $fortbildungName = $fortbildung->getName();
 
     foreach ($teilnehmer as $key) {
       $mail = $key->getEmail();
 
-    echo $mail.'<br>';
+
+      $to = strip_tags($mail);
+      $subject = strip_tags('Einladung zur Fortbildung: '.$fortbildungName);
+      $message = strip_tags($_POST['message']);//$_POST['message']
+
+
+
+      $mailer->From = "roccasalvo.lukas@hotmail.com";//Miriam.Bolognani@berufsschule.bz
+      $mailer-> addAddress($to, "Miriam Bolognani");
+      $mailer->Subject = $subject;
+      $mailer->Body = $message;
+
+      if (!$mailer->send()) {
+          echo "<p>failed to send mail</p></br />"
+          . "<p>" . $mailer->ErrorInfo . "</p>";
+      }
     }
 
-    $to = strip_tags('pincopallino@boo.com');
-    $subject = strip_tags('Einladung zur Fortbildung');
-    $message = strip_tags("hallo");//$_POST['message']
-
-
-
-    $mailer->From = "Miriam.Bolognani@berufsschule.bz";
-    $mailer-> addAddress($to, "Miriam Bolognani");
-    $mailer->Subject = $subject;
-    $mailer->Body = $message;
-
-    if (!$mailer->send()) {
-        echo "<p>failed to send mail</p></br />"
-        . "<p>" . $mailer->ErrorInfo . "</p>";
-    }
 
     /*
     $msg = $_POST['message'];
