62b8c57de2cd2441aa01721bec59b87f2a382ef9
MatRob0300 <matharobert1@gmail.com>
2020-02-17 14:43:12 +0100

    teilnerhmerliste htaccess hardcode

--- backend/views/teilnehmerliste.tpl.php
+++ backend/views/teilnehmerliste.tpl.php
@@ -1,3 +1,8 @@
+<?php
+if(!isset($_SESSION["loggedIn"])){
+  header('Location: ../index.php?aktion=login');
+}
+?>
 <head>
   <!-- https://t3n.de/news/css3-dynamische-tabs-ohne-365861/-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
