6b85458e74958783cabd2c1e22adde85b70cbde1
MatRob0300 <matharobert1@gmail.com>
2020-03-02 14:53:42 +0100

    formatierung kurs bearbeiten

--- backend/views/kurs_bearbeiten.tpl.php
+++ backend/views/kurs_bearbeiten.tpl.php
@@ -1,10 +1,4 @@
 
-<?php
-if(!isset($_SESSION["loggedIn"])){
-  header('Location: index.php?aktion=login');
-}
-?>
-
 <html>
 <head>
 
@@ -14,7 +8,11 @@ if(!isset($_SESSION["loggedIn"])){
   <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.15/dist/summernote.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.15/dist/summernote.min.js"></script>
 
-
+  <!--
+  $movie = array( "title" => "Rear Window",
+  "director" => "Alfred Hitchcock",
+  "year" => 1954 );
+  */-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="styles/kurs_bearbeiten-style.css">
   <link rel="stylesheet" type="text/css" href="styles/main-style.css">
@@ -34,16 +32,12 @@ if(!isset($_SESSION["loggedIn"])){
 
       <h1><?php echo $kurse->getTitel() ?> bearbeiten</h1>
     </header>
-
     <div id="kurserstelleninhalt">
       <div id="box">
           <form action="index.php?aktion=kurse_bearbeitung_speichern&fortbildung_id=<?php echo $_REQUEST['fortbildung_id']?>&kurs_id=<?php echo $_GET['kurs_id']?>"  method="post">
         <div id="input">
-          <label for="titel" class="labels">Titel</label>
-          <input type="text" name="titel" id="titel" value="<?php echo $kurse->getTitel()?>"/>
-
-          <label for="datum" class="labels">Datum</label>
-          <input type="date" name="datum" id="datum" value="<?php echo $kurse->getDatum()?>"/>
+          <p class="einzug"> Titel<input type="text" name="titel" id="titelkurs" value="<?php echo $kurse->getTitel()?>"/></p>
+          <p class="einzug"> Datum <input type="date" name="datum" id="datumkurs" value="<?php echo $kurse->getDatum()?>"/></p>
         </div>
         <p>Beschreibung</p>
 
@@ -61,40 +55,40 @@ if(!isset($_SESSION["loggedIn"])){
         </script>
 
 
+        <table id="kurszeiten">
+          <tr>
+            <td><a>Dauer:</a> &ensp;
+            <input type="number" name="dauer"  class="zeit" value="<?php echo $kurse->getDauer()?>" />
+          </td>
+          <td>
+            <a>Start:</a>  &ensp;
+            <input type="text" placeholder="10:00"  name='von' id="start" class="zeit" value="<?php echo $kurse->getVon()?>" />
+          </td>
+          <td>
+            &ensp; <a>Ende:</a>  &ensp;
+            <input type="text" placeholder="10:00"  name="bis" id="ende" class="zeit" value="<?php echo $kurse->getBis()?>"/>
+          </td>
+        </tr>
+      </table>
 
-          <p> Uhrzeit </p>
-          <label for="dauer" class="labels">Dauer</label>
-          <input type="number" name="dauer" id="dauer" class="zeit" value="<?php echo $kurse->getDauer()?>"/>
+      <div class=kursdaten>
+          <a> Koordination: </a>
+          <input type="text"  name="koordination" id="kurs_koordination_erstellen" value="<?php echo $kurse->getKoordination()?>"/>
+          <a> Anmeldeschluss: </a>
+        <input type="date" name="anmeldeschluss" id="kurs_anmeldeschluss_erstellen" value="<?php echo $kurse->getAnmeldeSchluss()?>">
+        <input type="hidden" id="beschreibung" name="beschreibung" />
+        <input type="hidden" id="fortbildung_id" name="fortbildung_id" value="<?php echo $_GET["fortbildung_id"]?>" />
+        <a> Teilnehmeranzahl: </a>
+        <input type="number" name="maxTeilnehmer" id="maxTeilnehmer" value="<?php echo $kurse->getTeilnehmerAnzahl()?>"/>
+        <a>Kontaktperson: </a>
+        <input type="text" name="kontakt" id="kontaktpersonen" value="<?php echo $kurse->getKontakt()?>"/>
+      <a> Referent: </a>
+      <input type="text"  name="referent" id="referent" value="<?php echo $kurse->getReferent()?>"/>
+        <a>Raum : </a>
+        <input type="text"  name="ort_raum" id="ort_raum" value="<?php echo $kurse->getOrt_raum()?>" />
 
-          <label for="von" class="labels">Von</label>
-          <input type="text"   name='von' id="start" class="zeit"  value="<?php echo $kurse->getVon()?>"/>
+</div>
 
-          <label for="bis" class="labels">Bis</label>
-          <input type="text"  name="bis" id="ende" class="zeit" value="<?php echo $kurse->getBis()?>"/><br/>
-
-
-
-            <label for="koordination" class="labels">Koordination</label>
-            <input type="text"  name="koordination" id="koordination" value="<?php echo $kurse->getKoordination()?>"/>
-
-            <label for="anmeldeschluss" class="labels">Anmeldeschluss</label>
-            <input type="date" name="anmeldeschluss" id="anmeldeschluss" value="<?php echo $kurse->getAnmeldeSchluss()?>">
-
-          <input type="hidden" id="beschreibung" name="beschreibung" />
-          <input type="hidden" id="fortbildung_id" name="fortbildung_id" value="<?php echo $_GET["fortbildung_id"]?>" />
-
-          <label for="maxTeilnehmer" class="labels">MaxTeilnehmer</label>
-          <input type="number" name="maxTeilnehmer" id="maxTeilnehmer" value="<?php echo $kurse->getTeilnehmerAnzahl()?>"/>
-
-          <label for="kontakt" class="labels">Kontakt</label>
-          <input type="text" name="kontakt" id="kontakt" value="<?php echo $kurse->getKontakt()?>"/>
-
-
-        <label for="referent" class="labels">Referent</label>
-        <input type="text"  name="referent" id="referent" value="<?php echo $kurse->getReferent()?>"/>
-
-          <label for="ort_raum" class="labels">Ort/Raum</label>
-          <input type="text"  name="ort_raum" id="ort_raum" value="<?php echo $kurse->getOrt_raum()?>"/>
 
 
 
