c9b66e8bdee85e413b1bc97059ba3e60c7e017f6
Andreas Codalonga <45843565+TheArled@users.noreply.github.com>
2021-05-18 10:12:39 +0200

    Added Calendar Event
    I added a link to the Confirmation Email to get a .ics file for adding an Event to the Calendar.

--- backend/models/funktionen.inc.php
+++ backend/models/funktionen.inc.php
@@ -1,5 +1,6 @@
 <?php
-class Funktionen{
+class Funktionen
+{
 
     public static function send_email()
     {
@@ -9,7 +10,7 @@ class Funktionen{
         $teilnehmer = NimmtTeil::findeAlleUnangemeldetenFortbildungTeilnehmer($fortbildung);
 
         $subject = strip_tags('Einladung zur Fortbildung: ' . $fortbildung->getName());
-        $message = strip_tags($_POST['message']);//$_POST['message']
+        $message = strip_tags($_POST['message']); //$_POST['message']
         if (is_array($teilnehmer)) {
             foreach ($teilnehmer as $key) {
                 $body_message = "\n \n Anmeldung unter:\n " . M_URL . "/" . M_URLUNTERORDNER . "/index.php?token=" . $key->gettoken() . "&aktion=login";
@@ -19,7 +20,8 @@ class Funktionen{
         }
     }
 
-    public static function send_email_teilnehmer(){
+    public static function send_email_teilnehmer()
+    {
         require_once 'PHPMailer-master/src/PHPMailer.php';
 
         $fortbildung = Fortbildung::finde($_REQUEST['fortbildung_id']);
@@ -27,7 +29,7 @@ class Funktionen{
         $teilnehmer = $kurs->getAllTeilnehmer();
 
         $subject = strip_tags('Mitteilung bezüglich der Fortbildung: ' . $fortbildung->getName());
-        $message = strip_tags($_POST['email_text']);//$_POST['message']
+        $message = strip_tags($_POST['email_text']); //$_POST['message']
         if (is_array($teilnehmer)) {
             foreach ($teilnehmer as $key) {
                 $body_message = "";
@@ -37,16 +39,16 @@ class Funktionen{
         }
     }
 
-  public static function importLehrer() {
+    public static function importLehrer()
+    {
 
         $csv_datei = $_FILES['datei']['tmp_name'];
         $name = $_FILES['datei']['name'];
-        $name = substr($name, 0, strlen($name) -4);
+        $name = substr($name, 0, strlen($name) - 4);
         $felder_trenner = ";";
         $zeilen_trenner = "\n";
 
         if (@file_exists($csv_datei) == false) {
-
         } else {
             $datei_inhalt = @file_get_contents($csv_datei);
             $zeilen = explode($zeilen_trenner, $datei_inhalt);
@@ -74,87 +76,77 @@ class Funktionen{
         return $teilnehmer_all;
     }
 
-    public static function send_bestaetigungs_email($kursId, $token) {
+    public static function send_bestaetigungs_email($kursId, $token)
+    {
         require_once 'PHPMailer-master/src/PHPMailer.php';
 
         error_reporting(E_ALL);
         $kurs = Kurs::finde($kursId);
         $teilnehmer = Teilnehmer::findeNachToken($token);
 
-        $subject = strip_tags('Anmeldung bei: '.$kurs->getTitel());
-        $message = strip_tags('Hiermit wird die Anmeldung zu '.$kurs->getTitel().' am '.$kurs->getDatum().' von '.$kurs->getVon().' bis '.$kurs->getBis().' im Raum/Ort '.$kurs->getOrt_raum().' bestätigt.');//$_POST['message']
-        $body_message = "\n \n Anmeldung ändern unter:\n ".M_URL."/".M_URLUNTERORDNER."/index.php?token=".$teilnehmer->getToken()."&aktion=login";
-    
+        $subject = strip_tags('Anmeldung bei: ' . $kurs->getTitel());
+        $message = strip_tags('Hiermit wird die Anmeldung zu ' . $kurs->getTitel() . ' am ' . $kurs->getDatum() . ' von ' . $kurs->getVon() . ' bis ' . $kurs->getBis() . ' im Raum/Ort ' . $kurs->getOrt_raum() . ' bestätigt.'); //$_POST['message']
+        $body_message = "\n \n Anmeldung ändern unter:\n " . M_URL . "/" . M_URLUNTERORDNER . "/index.php?token=" . $teilnehmer->getToken() . "&aktion=login" .
+            "\n \n In Kalender eintragen:\n " . M_URL . "/" . M_URLUNTERORDNER . "/index.php?kursId=" . $kursId . "&aktion=getCalendarItem";
+
         Funktionen::mailer($subject, $message, $body_message, $teilnehmer);
     }
 
-    public static function send_bestaetigungs_email_abmeldung($kursId, $token) {
+    public static function send_bestaetigungs_email_abmeldung($kursId, $token)
+    {
         require_once 'PHPMailer-master/src/PHPMailer.php';
 
         error_reporting(E_ALL);
         $kurs = Kurs::finde($kursId);
         $teilnehmer = Teilnehmer::findeNachToken($token);
 
-        $subject = strip_tags('Abmeldung bei: '.$kurs->getTitel());
-        $message = strip_tags('Hiermit wird die Abmeldung bei '.$kurs->getTitel().' am '.$kurs->getDatum().' von '.$kurs->getVon().' bis '.$kurs->getBis().' im Raum/Ort '.$kurs->getOrt_raum().' bestätigt.');//$_POST['message']
-        $body_message = "\n \n Neue Anmeldung unter:\n ".M_URL."/".M_URLUNTERORDNER."/index.php?token=".$teilnehmer->getToken()."&aktion=login";
+        $subject = strip_tags('Abmeldung bei: ' . $kurs->getTitel());
+        $message = strip_tags('Hiermit wird die Abmeldung bei ' . $kurs->getTitel() . ' am ' . $kurs->getDatum() . ' von ' . $kurs->getVon() . ' bis ' . $kurs->getBis() . ' im Raum/Ort ' . $kurs->getOrt_raum() . ' bestätigt.'); //$_POST['message']
+        $body_message = "\n \n Neue Anmeldung unter:\n " . M_URL . "/" . M_URLUNTERORDNER . "/index.php?token=" . $teilnehmer->getToken() . "&aktion=login";
 
         Funktionen::mailer($subject, $message, $body_message, $teilnehmer);
     }
 
 
 
-    static function mailer($subject, $message, $body_message, $teilnehmer){
+    static function mailer($subject, $message, $body_message, $teilnehmer)
+    {
         require_once("../entities/variables.ini.php");
 
         $mailer = new \PHPMailer\PHPMailer\PHPMailer();
-               
+
         // Nur für Entwicklung
-        if(M_MODE == "development"){
-                require_once('PHPMailer-master/src/Exception.php');
-                require_once('PHPMailer-master/src/SMTP.php');
-                $mailer->IsSMTP();
-                $mailer->Mailer = "smtp";
-                $mailer->SMTPDebug  = 0;  
-                $mailer->SMTPAuth   = TRUE;
-                $mailer->SMTPSecure = "tls";
-                $mailer->Port       = 587;
-                $mailer->Host       = "smtp.gmail.com";
-                $mailer->Username   = "berufsschulebozen.anmeldung@gmail.com";
-                $mailer->Password   = "Lbshi-12345";
+        if (M_MODE == "development") {
+            require_once('PHPMailer-master/src/Exception.php');
+            require_once('PHPMailer-master/src/SMTP.php');
+            $mailer->IsSMTP();
+            $mailer->Mailer = "smtp";
+            $mailer->SMTPDebug  = 0;
+            $mailer->SMTPAuth   = TRUE;
+            $mailer->SMTPSecure = "tls";
+            $mailer->Port       = 587;
+            $mailer->Host       = "smtp.gmail.com";
+            $mailer->Username   = "berufsschulebozen.anmeldung@gmail.com";
+            $mailer->Password   = "Lbshi-12345";
         }
         // Nur für Entwicklung
 
-                $mail = $teilnehmer->getEmail();
+        $mail = $teilnehmer->getEmail();
 
-                $to = strip_tags($mail);
+        $to = strip_tags($mail);
 
-                $mailer->From = "sekretariat@berufsschule.bz";
-                $mailer->FromName = "LBSHI Schule";
-                $mailer->addAddress($to, $teilnehmer->getVorname() . " " . $teilnehmer->getNachname());
-                $mailer->Subject = $subject;
-                $mailer->CharSet = "UTF-8";
-                $mailer->Body = $message . $body_message;
+        $mailer->From = "sekretariat@berufsschule.bz";
+        $mailer->FromName = "LBSHI Schule";
+        $mailer->addAddress($to, $teilnehmer->getVorname() . " " . $teilnehmer->getNachname());
+        $mailer->Subject = $subject;
+        $mailer->CharSet = "UTF-8";
+        $mailer->Body = $message . $body_message;
 
 
-                if (!$mailer->send()) {
-                    $_SESSION["Info_mail"] = "Fehler beim versenden ihrer Email!";
-                } else {
-                    $_SESSION["Info_mail"] = "Deine Email wurde erfolgreich versendet!";
-                }
+        if (!$mailer->send()) {
+            $_SESSION["Info_mail"] = "Fehler beim versenden ihrer Email!";
+        } else {
+            $_SESSION["Info_mail"] = "Deine Email wurde erfolgreich versendet!";
+        }
     }
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
- ?>
